d380302ee38f039d1638b723bde1bc81003bca8e
Kunal Sharma <xc9.workopen.common@gmail.com>
2018-01-15 16:18:47 +0530

    Update task_handler.js + index.css
    Added task completion task.
    Added a menu icon which will contain both delete as well completion options.

--- task_handler.js
+++ task_handler.js
@@ -8,7 +8,7 @@ function addTask(taskText, taskId) {
       return;
     }
   }
-  $('#task-list-cont ul').append('<li class="list-group-item" id="' + taskId + '">' + taskText + '<span class="delete-task-btn"><i class="fa fa-trash-alt"></i></span></li>');
+  $('#task-list-cont ul').append('<li class="list-group-item" id="' + taskId + '">' + taskText + '<div class="task-options-cont"><div class="dot-set dropdown" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div><div id="task-options-menu" class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2"><a class="complete-task-btn" class="dropdown-item" href="#"><span><i class="fa fa-check"></i></span>Task Complete</a><a class="delete-task-btn" class="dropdown-item" href="#"><span><i class="fa fa-trash-alt"></i></span>Delete Task</a></div></div></li>');
   attachTaskDeleteBtnListener();
 
   var taskInfo = taskId + ":" + taskText;
@@ -35,7 +35,8 @@ function attachTaskDeleteBtnListener() {
     $('.delete-task-btn').click(function () {
       // Remove error message if add task modal is open.
       $('#task-input-error-box').text("").slideUp(300).css('color', 'white');
-      var nodeId = $(this).parent().attr('id');
+      var nodeId = $(this).parent().parent().parent().attr('id');
+      console.log(nodeId);
       $('#' + nodeId).remove();
       deleteTaskFromStore(SESSION_STORE, nodeId);
     });
@@ -114,7 +115,7 @@ function loadTaskList(userId) {
     $('#task-list-cont ul').html('<li style="display: none;" id="task_0" class="list-group-item"><span class="delete-task-btn"><i class="fa fa-trash-alt"></i></span><!-- <input type="checkbox" taskid="task_0" class="select-task" /> --></li>');
     for (var i = 0; i < TASK_LIST.length; i++) {
       var data = fs.readFileSync('./data-store/user-store/' + userId + '/task-store-dir/' + TASK_LIST[i] + '.txt');
-      $('#task-list-cont ul').append('<li class="list-group-item" id="' + TASK_LIST[i] + '">' + data + '<span class="delete-task-btn"><i class="fa fa-trash-alt"></i></span></li>');
+      $('#task-list-cont ul').append('<li class="list-group-item" id="' + TASK_LIST[i] + '">' + data + '<div class="task-options-cont"><div class="dot-set dropdown" id="dropdownMenu2" data-toggle="dropdown" aria-expanded="false"><span class="dot"></span><span class="dot"></span><span class="dot"></span></div><div id="task-options-menu" class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2"><a class="complete-task-btn" class="dropdown-item" href="#"><span><i class="fa fa-check"></i></span>Task Complete</a><a class="delete-task-btn" class="dropdown-item" href="#"><span><i class="fa fa-trash-alt"></i></span>Delete Task</a></div></div></li>');
     }
     attachTaskDeleteBtnListener();
   }
