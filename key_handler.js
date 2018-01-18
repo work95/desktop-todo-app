b7f99d0a5fff76be526f26f93b467922d967eee7
Kunal Sharma <xc9.workopen.common@gmail.com>
2018-01-18 21:17:11 +0530

    Update key_handler.js
    1. Added OPEN_DROPDOWN_BOX variable for handling
    the dropdown input boxes (task input and project input).
    
    2. Shifted the code according to this state variable.

--- key_handler.js
+++ key_handler.js
@@ -7,6 +7,8 @@ const KeyCodes = {
   "ENTER": 13
 };
 
+var OPEN_DROPDOWN_BOX = 0;
+
 /* For performing operations when certain key combinations are pressed. */
 $(function () {
   var keyCombination = "";
@@ -15,7 +17,9 @@ $(function () {
     if (e.keyCode === KeyCodes.ESCAPE) {
       keyCombination = "";
       $('#task-add-input-box').slideUp(250).children('input').val("");
+      $('#project-add-input-box').slideUp(250).children('input').val("");
       $('#task-input-error-box').html("");
+      $('#project-input-error-box').html("");
     }
 
     if (e.keyCode === KeyCodes.CONTROL) {
@@ -23,15 +27,29 @@ $(function () {
     }
 
     keyCombination += e.keyCode;    // Concatenate the key code.
-    if (keyCombination.match(/^[1][7](82)+$/i) !== null) {
+    if (keyCombination.match(/^[1][7](82)+$/) !== null) {
       e.preventDefault();
     }
 
     // If the last key code forms 1723 (that means ctrl + enter is pressed together).
-    if (keyCombination.match(/^[1][7](13)+$/i) !== null) {
-      addTask($('#task-text-input').val(), 'task_' + new Date().getTime());
+    if (keyCombination.match(/^[1][7](13)+$/) !== null) {
+      if (OPEN_DROPDOWN_BOX === 1) {
+        if (LIST_CONT_STATE === 1) {
+          addTask($('#task-text-input').val(), 'task_' + new Date().getTime());
+        } else {
+          addProjectTask($('#task-text-input').val(), 'task_' + new Date().getTime());
+        }
+      } else if (OPEN_DROPDOWN_BOX === 2) {
+        addProject($('#project-text-input').val(), 'project_' + new Date().getTime());
+      }
     }
-    if (keyCombination == "1784") {
+
+    if (keyCombination === "1780") {
+      OPEN_DROPDOWN_BOX = 2;
+      $('#project-add-input-box').slideDown(250);
+      $('#project-text-input').focus();
+    } else if (keyCombination === "1784") {
+      OPEN_DROPDOWN_BOX = 1;
       $('#task-add-input-box').slideDown(250);
       $('#task-text-input').focus();
     }
