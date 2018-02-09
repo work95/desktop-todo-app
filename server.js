1c091ae2548c644e250eb5089391d99cf21e9625
Kunal Sharma <xc9.workopen.common@gmail.com>
2018-02-09 13:58:34 +0530

    Update server.js
    When a profile comes store them in user_list.txt file.
    Fixed typo in directory ./user_...
    
    Added request handler for '/getUser' endpoint.
    Checks to see if the user exists or not.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var bodyParser = require('body-parser');
 var express = require('express');
 var http = require('http');
@@ -20,11 +21,37 @@ server.listen(7910, function () {
 /* Complete profile (all tasks (project, normal), notes). */
 app.post('/portProfile', function (req, res) {
   var profile = req.body;
-  fs.writeFileSync('user_' + profile['userInfo']['userId'] + '.txt', JSON.stringify(profile));
+  fs.writeFileSync('./user_' + profile['userInfo']['userId'] + '.txt', JSON.stringify(profile));
+  fs.appendFileSync('./user_list.txt', profile['userInfo']['userId'] + '\n');
   
   res.sendStatus(200);
 });
 
 app.post('/getProfile', function (req, res) {
   res.send(fs.readFileSync('./user_' + req.body.userId + '.txt').toString());
+});
+
+app.post('/getUser', function (req, res) {
+  var userList = fs.readFileSync('./user_list.txt').toString().split('\n');
+  userList.pop();
+  let userId = "";
+
+  async.each(userList, function (user, callback) {
+    var userInfo = JSON.parse(fs.readFileSync('./user_' + user + '.txt').toString());
+    if (userInfo['userInfo']['userEmail'] === req.body.userEmail) {
+      callback(null);
+      userId = user;
+    } else {
+      userId = "";
+    }
+  }, function (err, result) {
+    console.log(err, result);
+    if (userId === "") {
+      res.sendStatus(404);
+    } else {
+      res.send(JSON.stringify({
+        "userId": userId
+      }));
+    }
+  });
 });
