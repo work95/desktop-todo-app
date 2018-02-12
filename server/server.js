3bb9f82cb521d3c00309e2fe249d7423a98623ab
Kunal Sharma <xc9.workopen.common@gmail.com>
2018-02-12 17:12:52 +0530

    Update server.js
    1. /poll - When profiles comes check if the user exists otherwise, the
    same userId will be appended again and again in the user_list.txt file.
    On each request arrival sync the profiles with other servers too.
    
    2. /sync - Used for receiving requests from other servers when
    synchronization of profiles's updated details is required. A
    requesting server will send the details (contain the profiles's data) and
    the receiving server will compare the portingTime (timestamp) of the
    already stored information with the arrived information, if stored
    information is stale it will be updated with the new details
    (OBVIOUSLY).

--- server/server.js
+++ server/server.js
@@ -4,14 +4,15 @@ const express = require('express');
 const http = require('http');
 const fs = require('fs');
 const logger = require('tracer').colorConsole();
+const request = require('request');
 
-var app = express();
+const app = express();
 
 app.disable('x-powered-by');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 server.listen(7910, function () {
 
   'use strict';
@@ -19,6 +20,8 @@ server.listen(7910, function () {
 });
 
 app.get('/poll', function (req, res) {
+
+  'use strict';
   res.send({
     "activity": "up",
     "systemType": "station"
@@ -27,26 +30,50 @@ app.get('/poll', function (req, res) {
 
 /* Store the profile in the server's directory. */
 app.post('/portProfile', function (req, res) {
-  var profile = req.body;
+
+  'use strict';
+  let profile = req.body['profile'];
+  if (!fs.existsSync('./user_list.txt')) {
+    fs.writeFileSync('./user_list.txt', "");
+  }
+
+  let userList = fs.readFileSync('./user_list.txt').toString().split('\n');
+
+  let userPresent = false;
+  for (let i = 0; i < userList.length; i++) {
+    if (profile['userInfo']['userId'] === userList[i]) {
+      userPresent = true;
+    }
+  }
+
+  if (!userPresent) {
+    fs.appendFileSync('./user_list.txt', profile['userInfo']['userId'] + '\n');
+  }
   fs.writeFileSync('./user_' + profile['userInfo']['userId'] + '.txt', JSON.stringify(profile));
-  fs.appendFileSync('./user_list.txt', profile['userInfo']['userId'] + '\n');
-  
+
+  // SYNC with other servers.
+  syncWithOtherServers();
+
   res.sendStatus(200);
 });
 
 /* Fetch the profile's information. */
 app.post('/getProfile', function (req, res) {
+
+  'use strict';
   res.send(fs.readFileSync('./user_' + req.body.userId + '.txt').toString());
 });
 
 /* Check if the user exists or not. */
 app.post('/getUser', function (req, res) {
-  var userList = fs.readFileSync('./user_list.txt').toString().split('\n');
+
+  'use strict';
+  let userList = fs.readFileSync('./user_list.txt').toString().split('\n');
   userList.pop();
   let userId = "";
 
   async.each(userList, function (user, callback) {
-    var userInfo = JSON.parse(fs.readFileSync('./user_' + user + '.txt').toString());
+    let userInfo = JSON.parse(fs.readFileSync('./user_' + user + '.txt').toString());
     if (userInfo['userInfo']['userEmail'] === req.body.userEmail) {
       callback(null);
       userId = user;
@@ -54,7 +81,6 @@ app.post('/getUser', function (req, res) {
       userId = "";
     }
   }, function (err, result) {
-    console.log(err, result);
     if (userId === "") {
       res.sendStatus(404);
     } else {
@@ -64,3 +90,80 @@ app.post('/getUser', function (req, res) {
     }
   });
 });
+
+app.post('/sync', function (req, res) {
+  let syncDetails = req.body;
+  for (let i = 0; i < syncDetails['userList'].length; i++) {
+    let userId = syncDetails['userList'][i];
+
+    // If user with this ID does exists, just create one.
+    if (!fs.existsSync('./user_' + userId + '.txt')) {
+      fs.writeFileSync('./user_' + userId + '.txt', syncDetails['userInfo']);
+      fs.appendFileSync('./user_list.txt', userId + '\n');
+      return;
+    }
+
+    let userInfo = JSON.parse(fs.readFileSync('./user_' + userId + '.txt').toString());
+    if (parseInt(userInfo['logTime']['portingTime']) < parseInt(JSON.parse(syncDetails['userInfo'][i])['logTime']['portingTime'])) {
+      fs.writeFileSync('./user_' + userId + '.txt', syncDetails['userInfo']);
+    }
+  };
+});
+
+function syncWithOtherServers() {
+  let servers = [
+    'http://127.0.0.1:7911/', 'http://192.168.1.2:7910/', 'http://192.168.1.3:7910/', 'http://192.168.1.4:7910/', 
+    'http://192.168.1.5:7910/', 'http://192.168.1.6:7910/', 'http://192.168.1.7:7910/', 'http://192.168.1.8:7910/'
+  ];
+  let availableServer = [];
+
+  async.every(servers, function (serverUrl, callback) {
+    sendRequest((serverUrl + 'poll'), 'GET', undefined, function (response) {
+      if (response.activity === "up") {
+        availableServer.push(serverUrl);
+      }
+      callback(null, true);
+    });
+  }, function (err, result) {
+    if (availableServer.length < 0) { return; }
+    
+    // Create sync details.
+    let syncDetails = {
+      "userList": [],
+      "userInfo": []
+    };
+
+    syncDetails['userList'] = fs.readFileSync('./user_list.txt').toString().split('\n');
+    syncDetails['userList'].pop();
+
+    for (let i = 0; i < syncDetails['userList'].length; i++) {
+      syncDetails['userInfo'].push(fs.readFileSync('./user_' + syncDetails['userList'][i] + '.txt').toString());
+    }
+    
+    for (let i = 0; i < availableServer.length; i++) {
+      sendRequest((availableServer[i] + 'sync'), 'POST', syncDetails, function (resp) {
+        logger.info(resp);
+      });
+    }
+  });
+}
+
+function sendRequest(url, method, bodyContent, callback) {
+  let options = {
+    "uri": url,
+    "method": method,
+    "headers": {
+      "content-type": 'application/json'
+    },
+    "body": JSON.stringify(bodyContent)
+  };
+
+  request(options, function (err, response, body) {
+    if (err) {
+      logger.warn('[Error]: ' + err);
+      callback({ "activity": "down" });
+    } else {
+      callback(JSON.parse(body));
+    }
+  });
+}
