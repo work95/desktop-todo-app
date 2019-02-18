d082b0d0d84b9ffb3ebe15a7acb6d66af5a581e3
Kunal Sharma <xc9.workopen.common@gmail.com>
2019-02-18 17:27:02 +0530

    Update Config.js
    1. Add NOTIFICATION_COUNT and
    SHOWN_NOTIFICATIONS.

--- Config.js
+++ Config.js
@@ -5,6 +5,9 @@ const List = require("./List");
 var TASK_LIST = new List();
 var USER_ID = "user_0";
 
+var SHOWN_NOTIFICATIONS = [];
+var NOTIFICATION_COUNT = 0;
+
 /* Common key codes. */
 const KeyCodes = {
   "ESCAPE": 27,
@@ -17,5 +20,7 @@ const KeyCodes = {
 module.exports = {
   TASK_LIST,
   USER_ID,
+  SHOWN_NOTIFICATIONS,
+  NOTIFICATION_COUNT,
   KeyCodes
 };
