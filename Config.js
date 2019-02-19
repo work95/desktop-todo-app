77f5a021182c3e3ace4d04ffdcfdb1a72a07a390
Kunal Sharma <xc9.workopen.common@gmail.com>
2019-02-19 13:09:34 +0530

    Add more configuration variables
    setupConfiguration() for setting up some variables that require USER_ID.

--- Config.js
+++ Config.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const env = require("dotenv").config();
 const List = require("./List");
 
 var TASK_LIST = new List();
@@ -7,6 +8,13 @@ var USER_ID = "user_0";
 
 var SHOWN_NOTIFICATIONS = [];
 var NOTIFICATION_COUNT = 0;
+
+const BASE_STORE_DIR = process.env.BASE_STORE_DIR;
+const USER_STORE_DIR = process.env.USER_STORE_DIR;
+const LAST_LOGIN_FILE = process.env.LAST_LOGIN_FILE;
+var TASK_STORE_DIR = process.env.TASK_STORE_DIR;
+
+var Tasks;
 
 /* Common key codes. */
 const KeyCodes = {
@@ -17,10 +25,19 @@ const KeyCodes = {
   "ENTER": 13
 };
 
-module.exports = {
+const Config = module.exports = {
   TASK_LIST,
+  Tasks,
   USER_ID,
   SHOWN_NOTIFICATIONS,
   NOTIFICATION_COUNT,
-  KeyCodes
+  KeyCodes,
+  LAST_LOGIN_FILE,
+  BASE_STORE_DIR,
+  USER_STORE_DIR,
+  TASK_STORE_DIR,
+
+  setupConfiguration: function () {
+    Config.TASK_STORE_DIR = `${Config.USER_STORE_DIR}/${Config.USER_ID}/${Config.TASK_STORE_DIR}`;
+  }
 };
