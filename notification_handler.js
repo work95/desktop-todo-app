239d4ad99a10b6672efc9bdc550374969af59e26
Kunal Sharma <xc9.workopen.common@gmail.com>
2019-02-19 19:06:52 +0530

    Refactored notification handler for notification pane removal.

--- notification_handler.js
+++ notification_handler.js
@@ -2,18 +2,17 @@ const countdown = require("countdown");
 const electron = require('electron');
 const {ipcRenderer} = require('electron');
 const Config = require("./Config");
-const UiIndex = require("./ui_index");
 const Utility = require("./Utility");
 
 module.exports = {
   checkNotifications: function () {
-    let list = Config.TASK_LIST.toKeyArray();
+    let list = Config.Tasks.getKeys()
     let endTimeList = {};
     let showFlag = 0;
     for (let i = 0; i < list.length; i++) {
       if (Config.SHOWN_NOTIFICATIONS.indexOf(list[i]) < 0) {
         showFlag++;
-        let time = Config.TASK_LIST.get(list[i])["taskEndTime"];
+        let time = Config.Tasks.getTask(list[i])["endTime"];
         if (time != null) {
           endTimeList[list[i]] = {
             endTime: time
@@ -45,19 +44,18 @@ function getTimeLeftObject(taskTimeLeft) {
   return countdown(new Date(), new Date(parseInt(taskTimeLeft)), units);
 }
 
-let lofo;
 function showNotifications(endTimeList) {
   let taskInfo = {};
   for (let i = 0; i < endTimeList.length; i++) {
     if (!isTaskNotified(endTimeList[i])) {
-      let info = Config.TASK_LIST.get(endTimeList[i]);
-      let timeSpan = getTimeLeftObject(info["taskEndTime"]);
+      let info = Config.Tasks.getTask(endTimeList[i]);
+      let timeSpan = getTimeLeftObject(info["endTime"]);
       if (timeSpan.hours <= 1 && timeSpan.value > 0) {
-        let date = new Date(parseInt(info["taskStartTime"]));
-        taskInfo["taskId"] = info["taskId"];
-        taskInfo["taskDate"] = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-        taskInfo["timeLeft"] = getTimeLeftString(info["taskEndTime"]);
-        taskInfo["taskText"] = decodeURIComponent(info["taskText"]);
+        let date = new Date(parseInt(info["startTime"]));
+        taskInfo["id"] = info["id"];
+        taskInfo["date"] = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+        taskInfo["left"] = getTimeLeftString(info["endTime"]);
+        taskInfo["text"] = decodeURIComponent(info["text"]);
         showNotification(taskInfo);
         return;
       }
@@ -66,32 +64,13 @@ function showNotifications(endTimeList) {
 }
 
 function showNotification(taskInfo) {
-  if (Config.NOTIFICATION_COUNT > 0) {
-    $('#notification-count-badge').fadeIn(200).text(Config.NOTIFICATION_COUNT);
-  }
-
-  $('#notification-pane ul').append(getNotificationTemplate(taskInfo));
-  Config.SHOWN_NOTIFICATIONS.push(taskInfo["taskId"]);
+  Config.SHOWN_NOTIFICATIONS.push(taskInfo["id"]);
 
   let screenSize = electron.screen.getPrimaryDisplay().size;
   ipcRenderer.send('notification-open',
     screenSize.width - 320,
     50,
-    taskInfo['timeLeft'],
-    (decodeURIComponent(taskInfo["taskText"]))
+    taskInfo['left'],
+    (decodeURIComponent(taskInfo["text"]))
   );
-}
-
-/* Template for notification element. */
-function getNotificationTemplate(taskInfo) {
-  let node = '' +
-    '<a href="#" id="notif_' + taskInfo["taskId"] + '" class="list-group-item list-group-item-action flex-column align-items-start">' +
-    '<div class="d-flex w-100 justify-content-between">' +
-    '<h5 class="mb-1">' + taskInfo["taskText"] + '</h5>' +
-    '<small class="text-muted">' + taskInfo["taskDate"] + '</small>' +
-    '</div>' +
-    '<small class="text-muted">' + taskInfo["timeLeft"] + '</small>' +
-    '</a>';
-
-  return node;
 }
