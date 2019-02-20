f7dc8a5b6442b12144e565631a4901d9df1db981
Kunal Sharma <xc9.workopen.common@gmail.com>
2019-02-20 17:05:49 +0530

    Global property for getting line number.

--- boot.js
+++ boot.js
@@ -3,3 +3,18 @@
 window.$ = window.jQuery = require('./assets/js/jquery-3.2.1.min.js');
 
 require("./index_main").init();
+
+/* Property for getting the stack trace. Used by below two properties. */
+Object.defineProperty(global, '__STACK__', {
+  get: function () {
+    var orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function (_, stack) {
+      return stack;
+    };
+    var err = new Error();
+    Error.captureStackTrace(err, this.get);
+    var stack = err.stack;
+    Error.prepareStackTrace = orig;
+    return stack;
+  }
+});
