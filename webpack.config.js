4095246d842a0416fddb0ecc80b34f3e1dc57911
Kunal Dutt Sharma <xc9.workopen.common@gmail.com>
2019-08-03 17:20:42 +0530

    Using single configuration now.

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,13 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 
-/* For the controller. */
-const config1 = {
-  entry: "./app/js/boot.js",
-  mode: "development",
-  output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "boot.js"
-  },
-  target: "node",
-  externals: [nodeExternals()],
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-				test: /\.css$/,
-				use: ["style-loader", "css-loader", {
-					loader: "postcss-loader",
-					options: {
-						plugins: () => [require("autoprefixer")]
-					}
-				}]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/"
-            }
-          }
-        ]
-      }
-    ]
-  }
-};
-
-/* For the view. */
-const config2 = {
+const config = {
   entry: "./app/js/index.js",
   mode: "development",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "start.js",
-    sourceMapFilename: "start.map"
+    filename: "main.js",
+    sourceMapFilename: "main.map"
   },
   devtool: "#source-map",
   target: "node",
@@ -96,4 +46,4 @@ const config2 = {
   }
 };
 
-module.exports = [config1, config2];
+module.exports = config;
